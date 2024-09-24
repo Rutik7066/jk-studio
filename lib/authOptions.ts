c3b9f178be40b62db80940a97b6689b63161a58b
Rutik7066 <rutikthakre@gmail.com>
2024-09-24 13:11:16 +0530

    config

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,27 +1,29 @@
+import Auth0 from "next-auth/providers/auth0";
 
 export const authOptions = {
   providers: [
-{
+    Auth0({
       id: "adobe",
       name: "Adobe",
-      clientId: process.env.ADOBE_CLIENT_ID as string,
+      clientId: process.env.ADOBE_CLIENT_ID || "",
+      clientSecret: process.env.ADOBE_CLIENT_SECRET || "",
       authorization: {
-        url: "https://ims-na1.adobelogin.com/ims/authorize/v2",
-        params: { response_type: "code", scope: "openid,lr_partner_apis" },
+        url: "https://ims-na1.adobelogin.com/ims/authorize",
+        params: {
+          response_type: "code",
+          scope: "openid,creative_sdk",
+        },
       },
-      token: "https://ims-na1.adobelogin.com/ims/token/v3",
-      userinfo: "https://ims-na1.adobelogin.com/ims/profile/v2",
-      wellKnown:
-        "https://ims-na1.adobelogin.com/ims/.well-known/openid-configuration",
-      profile(profile:{ sub: string; name: string; email: string; picture: string }) {
-        console.log(JSON.stringify(profile, null, 2));
+      token: "https://ims-na1.adobelogin.com/ims/token",
+      userinfo: "https://ims-na1.adobelogin.com/ims/userinfo",
+      profile(profile) {
         return {
           id: profile.sub,
           name: profile.name,
           email: profile.email,
-          image: profile.picture,
         };
       },
-    },
+    }),
   ],
+  secret: process.env.NEXTAUTH_SECRET || "",
 };
