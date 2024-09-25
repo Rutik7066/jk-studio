5311906932c25a4132888e4ee234aaa0053b61f6
Rutik7066 <rutikthakre@gmail.com>
2024-09-25 15:21:16 +0530

    cc
    Signed-off-by: Rutik7066 <rutikthakre@gmail.com>

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -9,7 +9,11 @@ export const authOptions = {
       clientSecret: process.env.ADOBE_CLIENT_SECRET || "",
       authorization: {
         url: "https://ims-na1.adobelogin.com/ims/authorize/v2",
-        params: { response_type: "code", scope: "openid,lr_partner_apis" },
+        params: {
+          response_type: "code",
+          scope:
+            "openid,creative_sdk,profile,address,AdobeID,email,cc_files,cc_libraries",
+        },
       },
       token: "https://ims-na1.adobelogin.com/ims/token/v3",
       userinfo: "https://ims-na1.adobelogin.com/ims/userinfo/v2",
@@ -18,18 +22,17 @@ export const authOptions = {
       profile(profile, token) {
         return {
           id: profile.sub,
-          profile: {...profile},
-          token :{ ...token},
+          profile: { ...profile },
+          token: { ...token },
         };
       },
     }),
   ],
   callbacks: {
     async jwt({ token, account }) {
-
       // If it's the initial sign-in, add the access token to the JWT
       if (account) {
-        console.log(JSON.stringify(account,null,2))
+        console.log(JSON.stringify(account, null, 2));
         token.accessToken = account.access_token;
         token.refreshToken = account.refresh_token;
         token.expires_at = account.expires_at;
